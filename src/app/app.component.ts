853cd3dce0789645d908b714959c3c900598147d
Lynn, Austin <alynn@nthrive.com>
2018-11-27 12:09:00 -0500

    cleaned up site, added web config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fieldstone',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fieldstone-jrs';
+  title = 'Fieldstone Juniors';
+  constructor(private router: Router) {
+
+  }
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
